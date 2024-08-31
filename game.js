e4358118399dc2829c60f5150698207ece8b6fe1
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2024-08-31 21:22:58 +0300

    sprint05 week02: tests and logic for moovings players and google

--- game.js
+++ game.js
@@ -1,14 +1,21 @@
 export class Game {
   #settings = {
+    pointsToWin: 10,
     gridSize: {
       columns: 4,
       rows: 4,
-    }
+    },
+    googleJumpInterval: 2000
   };
   #status = 'pending';
   #player1;
   #player2;
   #google;
+  #googleJumpIntervalId;
+  #score = {
+    1: {points: 0},
+    2: {points: 0}
+  }
 
   #getRandomPosition(existedPosition = []) {
     let newX;
@@ -17,35 +24,173 @@ export class Game {
     do {
       newX = NumberUtil.getRandomNumber(this.#settings.gridSize.columns);
       newY = NumberUtil.getRandomNumber(this.#settings.gridSize.rows)
-    } while (
-      existedPosition.some(pos => newX === pos.x && newY === pos.y)
-      )
+    } while (existedPosition.some(pos => newX === pos.x && newY === pos.y))
 
-    return new Position(newX, newY)
+    const position = new Position(newX, newY);
+    return position;
+  }
+
+  #moveGoogleToRandomPosition(excludeGoogle) {
+    let occupiedPositions = [this.#player1.position, this.#player2.position];
+
+    if (!excludeGoogle) {
+      occupiedPositions.push(this.#google.position);
+    }
+
+    this.#google = new Google(this.#getRandomPosition(occupiedPositions));
   }
 
   #createUnits() {
+    // const player1Position = this.#getRandomPosition([]);
     const player1Position = this.#getRandomPosition();
-    this.#player1 = new Player(1, player1Position);
+    this.#player1 = new Player(player1Position, 1);
 
     const player2Position = this.#getRandomPosition([player1Position]);
-    this.#player2 = new Player(1, player2Position);
+    this.#player2 = new Player(player2Position, 2);
 
-    const googlePosition = this.#getRandomPosition([player1Position, player2Position]);
-    this.#google = new Google(googlePosition);
+    this.#moveGoogleToRandomPosition(true);
   }
 
   async start() {
-    if (this.#status === 'pending') {
-      this.#status = 'in-process';
+    if (this.#status === "pending") {
+      this.#status = "in-process";
       this.#createUnits();
     }
+    this.#googleJumpIntervalId = setInterval(
+      () => this.#moveGoogleToRandomPosition(),
+      this.#settings.googleJumpInterval
+    );
   }
+
+  async stop() {
+    this.#status = "finished";
+    clearInterval(this.#googleJumpIntervalId);
+  }
+
+  #checkBorders(player, delta) {
+    const newPosition = player.position.clone();
+
+    if (delta.x) {
+      newPosition.x += delta.x;
+    }
+    if (delta.y) {
+      newPosition.y += delta.y;
+    }
+
+    if (
+      newPosition.x >= 1 ||
+      newPosition.x <= this.#settings.gridSize.columns
+    ) {
+      return false;
+    }
+    if (newPosition.y >= 1 || newPosition.y <= this.#settings.gridSize.rows) {
+      return false;
+    }
+    return true;
+  }
+
+  #checkAnotherPlayer(movingPlayer, anotherPlayer, delta) {
+    const newPosition = movingPlayer.position.clone();
+
+    if (delta.x) {
+      newPosition.x += delta.x;
+    }
+    if (delta.y) {
+      newPosition.y += delta.y;
+    }
+
+    return newPosition.equal(anotherPlayer.position);
+  }
+
+  #checkGoogleCatching(player) {
+    if (player.position.equal(this.#google.position)) {
+      this.#score[player.id].points++;
+
+      if (this.#score[player.id].points === this.#settings.pointsToWin) {
+        this.stop();
+        this.#google = new Google(new Position(0, 0));
+      }
+
+      this.#moveGoogleToRandomPosition();
+    }
+  }
+
+  #movePlayer(movingPlayer, anotherPlayer, delta) {
+    const isBorder = this.#checkBorders(movingPlayer, delta);
+    const isAnotherPlayer = this.#checkAnotherPlayer(
+      movingPlayer,
+      anotherPlayer,
+      delta
+    );
+    if (isBorder || isAnotherPlayer) {
+      return;
+    }
+
+    if (delta.x) {
+      movingPlayer.position = new Position(
+        movingPlayer.position.x + delta.x,
+        movingPlayer.position.y
+      );
+    } else {
+      movingPlayer.position = new Position(
+        movingPlayer.position.x,
+        movingPlayer.position.y + delta.y
+      );
+    }
+    this.#checkGoogleCatching(movingPlayer);
+  }
+
+  movePlayer1Right() {
+    const delta = {x: 1};
+    this.#movePlayer(this.#player1, this.#player2, delta);
+  }
+
+  movePlayer1Left() {
+    const delta = {x: -1};
+    this.#movePlayer(this.#player1, this.#player2, delta);
+  }
+
+  movePlayer1Up() {
+    const delta = {y: -1};
+    this.#movePlayer(this.#player1, this.#player2, delta);
+  }
+
+  movePlayer1Down() {
+    const delta = {y: 1};
+    this.#movePlayer(this.#player1, this.#player2, delta);
+  }
+
+  movePlayer2Right() {
+    const delta = {x: 1};
+    this.#movePlayer(this.#player2, this.#player1, delta);
+  }
+
+  movePlayer2Left() {
+    const delta = {x: -1};
+    this.#movePlayer(this.#player2, this.#player1, delta);
+  }
+
+  movePlayer2Up() {
+    const delta = {y: -1};
+    this.#movePlayer(this.#player2, this.#player1, delta);
+  }
+
+  movePlayer2Down() {
+    const delta = {y: 1};
+    this.#movePlayer(this.#player2, this.#player1, delta);
+  }
+
   get status() {
     return this.#status
   }
+
   set settings(settings) {
-    this.#settings = settings
+    // this.#settings = settings
+    this.#settings = {...this.#settings, ...settings};
+
+    this.#settings.gridSize = settings.gridSize
+      ? {...this.#settings.gridSize, ...settings.gridSize}
+      : this.#settings.gridSize;
   }
 
   get settings() {
@@ -59,8 +204,13 @@ export class Game {
   get player2() {
     return this.#player2
   }
+
   get google() {
     return this.#google
+  }
+
+  get score() {
+    return this.#score
   }
 }
 
@@ -71,7 +221,7 @@ class Unit {
 }
 
 class Player extends Unit {
-  constructor(id, position) {
+  constructor(position, id) {
     super(position)
     this.id = id;
   }
@@ -85,8 +235,16 @@ class Google extends Unit {
 
 class Position {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
+    this.x = x
+    this.y = y
+  }
+
+  clone() {
+    return new Position(this.x, this.y)
+  }
+
+  equal(anotherPosition) {
+    return anotherPosition.x === this.x && anotherPosition.y === this.y
   }
 }
 
